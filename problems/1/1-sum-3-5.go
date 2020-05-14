cce03f39548f591925ad6976ee5aa4372cee80b8
Shubhranshu <1248830+shubhranshu@users.noreply.github.com>
2020-05-14 16:19:15 +1200

    Move various files

--- problems/1/1-sum-3-5.go
+++ problems/1/1-sum-3-5.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	limit := 1000
 	sum := 0
-	for i:=1; i < limit; i++{
+	for i := 1; i < limit; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
 	fmt.Println(sum)
 }
-
